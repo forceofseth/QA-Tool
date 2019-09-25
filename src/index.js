2563fc1cd51b9960c116f08a3a0ce39c3a0014a1
Lukas Kiefer <forceofseth@gmail.com>
2019-09-25 17:28:12 +0200

    adds redux persistance to the application

--- src/index.js
+++ src/index.js
@@ -3,17 +3,24 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
-import {createStore, applyMiddleware, combineReducers} from 'redux';
+import {createStore, applyMiddleware} from 'redux';
 import thunk from 'redux-thunk';
-import firebaseReducer from "./redux/reducers/firebaseReducer";
 import {Provider} from "react-redux";
+import {rootReducer} from "./redux/reducers/rootReducer";
+import {persistStore, persistReducer} from 'redux-persist';
+import {persistConfig} from "./redux/persistConfig";
+import {PersistGate} from 'redux-persist/lib/integration/react';
+import Loading from "./components/Loading";
 
 
-const rootReducer = combineReducers({firebase: firebaseReducer});
-const store = createStore(rootReducer, applyMiddleware(thunk));
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+const store = createStore(persistedReducer, applyMiddleware(thunk));
+const persistor = persistStore(store);
 ReactDOM.render(
     <Provider store={store}>
-        <App/>
+        <PersistGate loading={<Loading/>} persistor={persistor}>
+            <App/>
+        </PersistGate>
     </Provider>
     ,
     document.getElementById('root'));
