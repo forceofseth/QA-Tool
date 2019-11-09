0e0461c5ee85adf5f69ceb35511a590f6bcd92e6
Lukas Kiefer <forceofseth@gmail.com>
2019-11-09 16:43:29 +0100

    deleteed one argument from createStore

--- src/redux/reducers/caseReducer.js
+++ src/redux/reducers/caseReducer.js
@@ -3,12 +3,13 @@ import {CREATE_CASE, CREATE_CASE_ERROR} from "../actions/caseActions";
 const initalState = {};
 
 export default function caseReducer(state = initalState, action) {
+    //TODO Lukas should we alter the state?
     switch (action.type) {
         case CREATE_CASE:
-            console.log("created case", action.payload)
+            console.log("created case", action.payload);
             return state;
         case CREATE_CASE_ERROR:
-            console.log("create case error", action.payload)
+            console.log("create case error", action.payload);
             return state;
         default:
             return state;
