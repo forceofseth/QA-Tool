66e608a5cf51e9aac5406119507a76fa813b827c
forceofseth <forceofseth@gmail.com>
2019-11-21 23:42:18 +0100

    waiting with redirection till firebase auth data is loaded. show spinner while firebase auth is not loaded.

--- src/components/Ui/PrivateRoute.js
+++ src/components/Ui/PrivateRoute.js
@@ -1,22 +1,23 @@
 import React from "react";
-import {Route, Redirect} from "react-router-dom";
-import {getAuth} from "../../redux/selectors";
+import {Route} from "react-router-dom";
 import {SIGN_IN} from "../../constants/routes";
-import {connect} from "react-redux";
+import {Redirect} from "react-router-dom";
+import {getAuth} from "../../redux/selectors";
+import connect from "react-redux/lib/connect/connect";
+import Loading from "../Status/Loading";
 
 const PrivateRoute = ({component: RouteComponent, auth, ...rest}) => {
-    return (
-        <Route
-            {...rest}
-            render={routeProps =>
-                !auth.isLoaded && auth.isEmpty ? (
+    return <Route
+        {...rest}
+        render={routeProps =>
+            !auth || !auth.isLoaded ? <Loading/> : (
+                auth.isEmpty ?
                     <Redirect to={SIGN_IN}/>
-                ) : (
+                    :
                     <RouteComponent {...routeProps} />
-                )
-            }
-        />
-    );
+            )
+        }
+    />;
 };
 
 const mapStateToProps = state => {
